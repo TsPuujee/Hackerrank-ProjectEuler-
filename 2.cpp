eee01b793d88ea5f59b491be00d4c69e8ee741f5
Пүрэвжаргал <puujeep60@gmail.com>
2018-04-20 11:19:16 +0800

    Project Euler #2: Even Fibonacci numbers

--- 2.cpp
+++ 2.cpp
@@ -1,21 +1,20 @@
 #include<bits/stdc++.h>
-
 using namespace std;
-vector<long long>F(15);
+vector<unsigned long long>F(30);
 
 int main(){
     int t;
     cin >> t;
      F[0]=2; F[1]=8;
-    for(int ii=2;ii<15;ii++)
+    for(int ii=2;ii<30;ii++)
         F[ii]=F[ii-1]*4+F[ii-2];
     
     for(int a0 = 0; a0 < t; a0++){
         long n;
         cin >> n;
-        int ans=0,i=0;
+        unsigned long long ans=0,i=0;
         do{
-            int t=F[i];
+            unsigned long long t=F[i];
             if(t>n) break;
             ans=ans+t;
             i++;
